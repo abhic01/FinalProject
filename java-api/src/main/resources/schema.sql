6d9db350b7dcc21dff8365338bc6100a3beccee6
abhic01 <abhi19@vt.edu>
2023-08-04 15:43:08 +0100

    Added Users.java

--- java-api/src/main/resources/schema.sql
+++ java-api/src/main/resources/schema.sql
@@ -1,43 +1,38 @@
 DROP TABLE IF EXISTS Security;
 CREATE TABLE Security (
-    id INT AUTO_INCREMENT,
-    isin VARCHAR(50),  
-    cusip VARCHAR(50),
-    issuer_name VARCHAR(255) NOT NULL,
-    maturity_date DATE NOT NULL,
-    coupon FLOAT NOT NULL,
-    type VARCHAR(255) NOT NULL,
-    face_value float NOT NULL,
-    currency varchar(10) NOT NULL,
-    status varchar(32),
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE Security_User (
-  id int AUTO_INCREMENT,
-  name varchar(255) NOT NULL,
-  email varchar(255) NOT NULL,
-  role varchar(255) NOT NULL,
+  id INT AUTO_INCREMENT,
+  isin VARCHAR(50),  
+  cusip VARCHAR(50),
+  issuer_name VARCHAR(255) NOT NULL,
+  maturity_date DATE NOT NULL,
+  coupon FLOAT NOT NULL,
+  type VARCHAR(255) NOT NULL,
+  face_value float NOT NULL,
+  currency varchar(10) NOT NULL,
+  status varchar(32),
   PRIMARY KEY (id)
 );
 
+DROP TABLE IF EXISTS Users;
+CREATE TABLE Users (
+  id INT AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  role VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS Book;
 CREATE TABLE Book (
   id INT AUTO_INCREMENT,
   issuer_name VARCHAR(255) NOT NULL,
   PRIMARY KEY (id)
 );
 
-
+DROP TABLE IF EXISTS Book_User;
 CREATE TABLE  Book_User(
-    book_id INT NOT NULL,
-    user_id INT NOT NULL,
-    FOREIGN KEY (book_id) REFERENCES Book(id),
-    FOREIGN KEY (user_id) REFERENCES Security_User(id)
+  book_id INT NOT NULL,
+  user_id INT NOT NULL,
+  FOREIGN KEY (book_id) REFERENCES Book(id),
+  FOREIGN KEY (user_id) REFERENCES Users(id)
 );
-
-
-
-
-
-
-
