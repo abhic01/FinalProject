c0c289650b41b8664fda182eb73d621b643833f6
AnuDB <vashisthazan@gmail.com>
2023-08-04 21:21:13 +0100

    just add queries and bond_holder column

--- java-api/src/main/resources/extra.sql
+++ java-api/src/main/resources/extra.sql
@@ -25,3 +25,30 @@ users.name = 'Mark Cuban';
 
 
 
+SELECT* from Security
+WHERE maturity_date < CURRENT_DATE() 
+AND status = 'active'
+
+
+SELECT * 
+FROM Security
+WHERE DATEDIFF('DAY', CURRENT_DATE(), maturity_date) > 0
+AND DATEDIFF('DAY', CURRENT_DATE(), maturity_date) < 6
+
+
+SELECT* from Security
+WHERE status='redeeming'
+
+
+SELECT* from Security
+WHERE maturity_date = CURRENT_DATE()
+
+
+SELECT* from Security
+WHERE DATEDIFF('DAY', CURRENT_DATE(), maturity_date) < 0
+AND DATEDIFF('DAY', CURRENT_DATE(), maturity_date) < 6
+
+
+SELECT bond_holder, COUNT(*) 
+FROM Security
+GROUP BY bond_holder
