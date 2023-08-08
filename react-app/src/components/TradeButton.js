c3d6a24a92d2b1245c1260b0baada486b83f061e
dylan1742 <dylanmccarthy2013@icloud.com>
2023-08-08 15:34:39 +0100

    trade button

---
+++ react-app/src/components/TradeButton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// const StyledButton = styled.TradeButton`
+//   background-color: #007bff;
+//   color: #fff;
+//   padding: 10px 20px;
+//   border: none;
+//   border-radius: 4px;
+//   cursor: pointer;
+// `;
+
+export const TradeButton = () => {
+  return (
+      <button>Sell</button>
+	);
+};
